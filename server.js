7798f4f07d3a909f83cfd5cae7d7b9099932b6c0
DeadWolf99 <nagabhagya82@gmail.com>
2020-04-20 20:29:23 +0530

    Added server close conditions

--- server.js
+++ server.js
@@ -1,15 +1,34 @@
 const express = require('express');
 const bp = require('body-parser');
 const cors = require('cors');
-const PORT = 8125
+const PORT = process.env.port || 8125
 const app = express();
 
 app.use(bp.json());
 app.use(cors());
 
 //Test comment
-app.listen(process.env.port || PORT, () => {
+var server = app.listen(PORT, () => {
     console.log('Application listening on port: ' + PORT);
+});
+
+process
+  .on('unhandledRejection', (reason, p) => {
+    console.error(reason, 'Unhandled Rejection at Promise', p);
+    server.close();
+  })
+  .on('uncaughtException', err => {
+    console.error(err, 'Uncaught Exception thrown');
+    server.close();
+    process.exit(1);
+  })
+  .on('SIGTERM', err=> {
+      server.close();
+      process.exit(1);
+  })
+  .on('SIGINT', err=> {
+    server.close();
+    process.exit(1);
 });
 
 app.use('/',require('./routes/test.js'));
